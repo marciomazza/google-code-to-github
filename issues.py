3fa18a5ad05f294f364464c038a8d3e4cb063f03
Marcio Mazza <marciomazza@gmail.com>
2012-12-08 13:40:41 -0200

    Scrap attachments from google code
    Google Code API does not allow attachements retrieval now:
    http://code.google.com/p/support/issues/detail?id=3213

--- issues.py
+++ issues.py
@@ -1,11 +1,14 @@
 import getpass
 import itertools
+import re
 from datetime import datetime
 
 import gdata.projecthosting.client
+from gdata.projecthosting.client import Query
+from lxml import html
 
 
-class Bunch:
+class Bunch(object):
     "http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named"
 
     def __init__(self, **kwds):
@@ -14,14 +17,43 @@ class Bunch:
     def __repr__(self):
         return self.__dict__.__repr__()
 
+class Attachment(object):
+
+    def __init__(self, node):
+        self.node = node
+        self.url = next(a.attrib['href'] for a in node.cssselect('a')
+                        if a.text == 'Download')
+        parent = node.getparent()
+        if 'issuedescription' in parent.attrib['class']:
+            self.type, self.index = 'description', None
+        elif 'issuecomment' in parent.attrib['class']:
+            comment_id =int(re.search(r'\d+$',
+                                      parent.attrib['id']).group())
+            self.type, self.index = 'comment', comment_id
+        else:
+            raise AssertionError('Unrecognized attachment %s' % node)
+
+    def __repr__(self):
+        d = self.__dict__.copy()
+        del d['node']
+        return d.__repr__()
+
 class Issue(Bunch):
-    pass
+
+    @property
+    def scrap(self):
+        if not hasattr(self, '_scrap') or not self._scrap:
+            self._scrap = html.parse(self.url).getroot()
+        return self._scrap
+
+    @property
+    def attachments(self):
+        return map(Attachment, self.scrap.cssselect('.attachments'))
 
 def step_queries(max_query_results):
     for start_index in itertools.count(start=1, step=max_query_results):
-        yield gdata.projecthosting.client.Query(
-            start_index = start_index,
-            max_results = max_query_results)
+        yield Query(start_index = start_index,
+                    max_results = max_query_results)
 
 class GoogleCodeProject(object):
 
@@ -38,24 +70,29 @@ class GoogleCodeProject(object):
             password = password or getpass.getpass("Type the google password for %s" % email)
             self.client.client_login(email, password, 'migration')
 
-    def issues(self):
-        for query in step_queries(self.max_query_results):
+    def get_issues(self, query=None):
+        queries = [query] if query else step_queries(self.max_query_results)
+        for query in queries:
             feed = self.client.get_issues(self.name, query = query)
             if feed.entry:
                 for issue in feed.entry:
+                    url = issue.link[1].href
                     yield Issue(
                         id = int(issue.id.text.split('/')[-1]),
                         status = issue.status.text.lower() if issue.status else None,
                         title = issue.title.text,
-                        link = issue.link[1].href,
+                        url = url,
                         authors = [a.name.text for a in issue.author],
                         content = issue.content.text,
                         date = datetime.strptime(
                             issue.published.text, "%Y-%m-%dT%H:%M:%S.000Z"),
                         labels = [l.text for l in issue.label],
                         owner = issue.owner.username.text if issue.owner else None,
-                        feed_entry = issue,
+                        raw = issue,
                         )
             else:
                 break
 
+    def get_issue_by_id(self, issue_id):
+        issues = list(self.get_issues(Query(issue_id=issue_id)))
+        return issues[0] if issues else None
